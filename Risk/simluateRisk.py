a0fabb5f89047a3c4ac6361185f117f472d00bd1
ujeab <ujeab@student.kit.edu>
2025-07-20 17:49:13 +0200

    container file, simulation saterted

---
+++ Risk/simluateRisk.py
@@ -0,0 +1,11 @@
+import battle_round as br
+
+def simulateRisk():
+    # Wie Daten darstellen? 
+
+    # wie viel RFuns pro Runde 
+    runs = 1000000
+    pass
+
+if __name__ == "__main__":
+    simulateRisk()
