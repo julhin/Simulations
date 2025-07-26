342134f4c10445528641f4840df8a6246eee5632
ujeab <ujeab@student.kit.edu>
2025-07-26 20:50:57 +0200

    simuöation algo dev

--- Risk/battle_round.py
+++ Risk/battle_round.py
@@ -7,14 +7,15 @@ class BattleRound:
     
     def fight(self, to_the_end = True):
         dice = dice_rolls.Dice()
-        winner = ""
+        #winner = ""
+        winnerSide = ""
         armiesLeft = 0
         while(self.attackerArmies > 0 and self.defenderArmies > 0):
             # fight a round 
             atk_dice = dice.roll_dice(self.getDice(self.attackerArmies), True)
             def_dice = dice.roll_dice(self.getDice(self.defenderArmies), True)
 
-            print(atk_dice, def_dice)
+            #print(atk_dice, def_dice)
             atk_casualties, def_casualities = 0, 0
             for idx in range(0, min(len(def_dice),len(atk_dice))):
                 if (atk_dice[idx] > def_dice[idx]):
@@ -23,16 +24,19 @@ class BattleRound:
                     atk_casualties += 1
             self.attackerArmies -= atk_casualties
             self.defenderArmies -= def_casualities
-            print(f"atk army loss: {atk_casualties} def army loss: {def_casualities}")
+           
+            #print(f"atk army loss: {atk_casualties} def army loss: {def_casualities}")
             if (self.attackerArmies <= 0):
-                winner = f"Defender won with {self.defenderArmies} left"
+                #winner = f"Defender won with {self.defenderArmies} left"
                 armiesLeft = self.defenderArmies
+                winnerSide = "def"
             if (self.defenderArmies <= 0):
-                winner= f"Attacker won with {self.attackerArmies} left"   
+                #winner= f"Attacker won with {self.attackerArmies} left"   
                 armiesLeft = self.attackerArmies
+                winnerSide = "atk"
         # DB Access: Winner und verbleibende Armeen
-        print(winner)
-        return (winner, armiesLeft)
+        #print(f"{winnerSide} : {armiesLeft}")
+        return (winnerSide, armiesLeft)
 
 
     def getDice(self,armies):
--- Risk/simluateRisk.py
+++ Risk/simluateRisk.py
@@ -1,11 +1,30 @@
 import battle_round as br
+import pandas as pd 
 
 def simulateRisk():
     # Wie Daten darstellen? 
 
-    # wie viel RFuns pro Runde 
+    # wie viel Runs pro Runde 
     runs = 1000000
-    pass
+    runs = 1
+    max_armies = 30
+    result = {}
+    # verschachtelte Schleife und Tupel - Spaß :)
+    result = {}
+    for attackerArmies in range(0, max_armies):
+        for defenderArmies in range(0,max_armies):
+            attackerList = []
+            defenderList = []
+            for _ in range(0, runs):
+                battle = br.BattleRound(attackerArmies, defenderArmies)
+                battleResult = battle.fight()
+                if (battleResult[0] == 'def'):
+                    defenderList.append(battleResult[1])
+                if(battleResult[0] == 'atk'):
+                    attackerList.append(battleResult[1])
+                
+                result[(attackerArmies, defenderArmies)][battleResult[0]].append(battleResult[1])
+    print(result)
 
 if __name__ == "__main__":
     simulateRisk()
